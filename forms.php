0f89b755666cb103451d27e7301ee8aa1fdb58e0
Barry Nesbit <barry@nesbit.net>
2015-04-20 18:52:39 -0400

    forms.php prelimin complete

--- forms.php
+++ forms.php
@@ -1,33 +1,52 @@
+<?php
+//prevent direct access (sort of)
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  header("Location: http://www.pinebluffretreat.com");
+  exit;
+};
+
+// setup email params
+$to = "barry-pbhandler@nesbit.net";
+$subject = "Pine Bluff " . $_POST['formName'] . " - " . $_POST['last'];
+$message = "
 <html>
+<head>
+<title>Contact Us</title>
+</head>
 <body>
-Form handler.
-
-<p><?php print "CONTENT_TYPE: " . $_SERVER['CONTENT_TYPE'] . "<BR />"; ?>
-<p>
-Post data:<?php print_r($_POST); ?>
-<p>
-Get data: <?php print_r($_GET); ?>
-
-<table>
-<?php 
-
-
-    foreach ($_POST as $key => $value) {
-        echo "<tr>";
-        echo "<td>";
-        echo $key;
-        echo "</td>";
-        echo "<td>";
-        echo $value;
-        echo "</td>";
-        echo "</tr>";
-    }
-
-
-?>
+<table>";
+foreach ($_POST as $key => $value) {
+    $message .= "<tr>";
+    $message .= "<td>";
+    $message .= $key;
+    $message .= "</td>";
+    $message .= "<td>";
+    $message .= $value;
+    $message .= "</td>";
+    $message .= "</tr>";
+};
+$message .= "
 </table>
 </body>
-</html>
+</html>";
 
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: <barry-pb@nesbit.net>' . "\r\n";
 
+// send email
+mail($to,$subject,$message,$headers);
+http_response_code(200);
+echo "Thank You! Your message has been sent.";
 
+// debug output
+/*
+echo "Subject: ".$subject."<br>";
+echo "To: ".$to."<br>";
+echo "Message: ".$message."<br>";
+*/
+?>
+<script>
+alert("hit form");
+</script>
